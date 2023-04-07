c9357ed8df80d81d1978dae2822aa939b09afeb4
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-04-07 13:53:55 +0500

    Initial commit

---
+++ src/test/java/graphql/learning/GraphqlLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package graphql.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
